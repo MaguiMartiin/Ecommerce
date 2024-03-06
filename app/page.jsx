9e5a183ec3af3e410054d08d6504e4f12d8e5209
Magui Martin <magalimartin02@gmail.com>
2024-03-05 21:41:11 -0300

    Filtro categoria Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,35 @@
-export default function Home() {
+import axios from "axios"
+import Link from "next/link";
+
+const fetchCategories = async () => {
+  try {
+    return (await axios.get('http://localhost:3001/categories')).data;
+  } catch (error) {
+    console.error('Error en fetchCategories:', error);
+    throw new Error(error.message);
+  }
+};
+export default async function Home() {
+  const categories = await fetchCategories()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1>Esto es el Home</h1>
+    <main className="flex flex-wrap items-center bg-slate-400">
+      {categories.slice(0,6).map(category => (
+        <div key={category.id} className="w-1/3" >
+        <Link href={`/product-category/${category.id}`}>
+          {category.Products.length > 0 ? (
+            <div className="flex" key={category.id}>
+              {category.Products.slice(0,1).map(product => (
+                  <div className="flex flex-col" key={product.id}>
+                    <img src={product.image} alt={product.name} className="w-full h-[50rem] object-cover"/>
+                    <h1 className="flex mt-[5rem] absolute text-white font-bold text-4xl">{category.name}</h1>
+                  </div>
+              ))}
+            </div>
+          ) : ( null )}
+        </Link>
+        </div>
+      ))}
     </main>
   )
 }
--- components/ModalCategories.jsx
+++ components/ModalCategories.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function ModalCategories({ categories, onMouseEnter, onMouseLeave }) {
   return (
-    <div className="fixed w-1/4 h-1/2 text-black rounded bg-white ml-[5rem] mt-[0.5rem] p-[1rem] shadow-xl"
+    <div className="fixed w-1/4 h-1/2 text-black rounded bg-white mt-[0.5rem] p-[1rem] shadow-xl z-10"
       onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       {categories && categories.map((e, index) => (
         <Link key={index} className="flex mb-[0.5rem] hover:text-red-700" href={`/product-category/${e.id}`}>
