624d4c23b50de013c274b9c083babc7cb2147af1
Magui Martin <magalimartin02@gmail.com>
2024-03-28 19:32:57 -0300

    arreglo inicio

--- app/product-category/[id]/page.jsx
+++ app/product-category/[id]/page.jsx
@@ -13,23 +13,26 @@ export default async function ProductCategoryId (query) {
         }
     }
     const products = await fetchProductCategory(categoryId)
+    const categoryName = products.length > 0 ? products[0].Categories[0]?.name : '';
 
     return(
         <div>   
-            {products.map(product => (
-                <div key={product.id}>
-                    <div className="p-[2rem]">
-                        <h1 className="flex text-2xl ">{product.Categories[0]?.name}</h1>
-                        <div className=" flex text-sm mt-[0.5rem] space-x-3 items-center">
-                            <h1>Inicio</h1>
-                            <FontAwesomeIcon icon={faAngleRight} />
-                            <h1>{product.Categories[0]?.name} </h1>
-                        </div>
-                    </div>
-                    <img src={product.image} alt={product.name} className=" w-96 h-96 object-cover" />
-                    <h2 className="flex p-[1rem]">{product.name}</h2>
-                </div>       
-            ))}
+            <div className="p-[2rem]">
+                <h1 className="flex text-2xl ">{categoryName}</h1>
+                <div className=" flex text-sm mt-[0.5rem] space-x-3 items-center">
+                    <h1>Inicio</h1>
+                    <FontAwesomeIcon icon={faAngleRight} />
+                    <h1>{categoryName}</h1>
+                </div>
+            </div>
+            <div className="flex flex-wrap space-x-1">
+                {products.map(product => (
+                    <div key={product.id}>
+                        <img src={product.image} alt={product.name} className=" w-96 h-96 object-cover" />
+                        <h2 className="flex p-[1rem]">{product.name}</h2>
+                    </div>       
+                ))}
+            </div>
         </div>
     )
 }
