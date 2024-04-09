13919c2f7c6d616ac50d00787cf57638a834478f
Magui Martin <magalimartin02@gmail.com>
2024-04-09 16:27:45 -0300

    agregando funcionalidad al search

---
+++ app/shop/search/page.jsx
@@ -0,0 +1,44 @@
+import axios from "axios"
+import Link from "next/link"
+
+const fetchProductSearch = async (name) => {
+    try {
+        return (await axios.get(`http://localhost:3001/products/?name=${name}`)).data
+    } catch (error) { throw new Error(error.message)}
+}
+
+const fetchProductSearchCategory = async (id, name) => {
+    try {
+        return (await axios.get(`http://localhost:3001/categories/?categoryId=${id}&name=${name}`)).data
+    } catch (error) { throw new Error(error.message)}
+}
+
+export default async function Search (query) {
+    const {name} = query.searchParams
+    const { id } = query.searchParams
+    let products = []
+
+    if (id) {
+        products = await fetchProductSearchCategory(id, name)
+      } else {
+        products = await fetchProductSearch(name)
+    }
+    
+    return(
+        <div className="flex items-center">
+            <div className="grid grid-flow-row gap-1 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 ">
+                {products.map(product => (
+                    <Link key={product.id} href='/shop/[id]' as={`/shop/${product.id}`}>
+                        <div>
+                            <img src={product.image} alt={product.name} className=" w-96 h-96 object-cover" />
+                            <div className="p-[1rem] mb-[1rem]">   
+                                <h2 className="mb-[0.2rem] text-lg">{product.name}</h2>
+                                <h2 className="text-sm">{product.Categories?.name}</h2>
+                            </div>
+                        </div>                        
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
--- components/ModalSearch.jsx
+++ components/ModalSearch.jsx
@@ -1,29 +1,55 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faXmark, faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-export function ModalSearch ({categories, handleCloseModal}) {
-    return (
-        <div className="fixed top-0 left-0 z-50 w-full h-full bg-gray-800 bg-opacity-50 flex justify-end overflow-hidden">
-            <div className="w-1/4 p-[1rem] bg-white">
-                <div className="flex items-center mb-[2rem]">
-                    <h1 className="text-xl flex-1 text-center">Buscar...</h1>
-                    <FontAwesomeIcon icon={faXmark} className="ml-auto w-3 cursor-pointer" onClick={handleCloseModal} />
-                </div>
-                <div className="border p-[1rem] mb-[1rem]">
-                    <select className="w-full focus:outline-none"> 
-                        <option value="default">Todas las categorias</option>
-                        {categories.map((e) => (
-                            <option value={e.id} key={e.id}>{e.name}</option>
-                        ))}       
-                    </select>
-                </div>
-                <div className="border p-[1rem] mb-[1rem]">
-                    <form className="flex items-center">
-                        <input type="text" placeholder="Buscar artículos" className="focus:outline-none" />
-                        <FontAwesomeIcon icon={faSearch} className="ml-auto w-4" />
-                    </form>
-                </div>
-            </div>
+export function ModalSearch({ categories, handleCloseModal }) {
+  const [selectedCategory, setSelectedCategory] = useState("default")
+  const [searchText, setSearchText] = useState("")
+  
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value.trim())
+  }
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault()
+    if (selectedCategory === 'default' && searchText !== "") {
+      window.location.href = `/shop/search?name=${searchText}`
+    }
+    else if (selectedCategory !== 'default' && searchText !== "") {
+      window.location.href = `/shop/search?id=${selectedCategory}&name=${searchText}`
+    }
+  }
+
+  return (
+    <div className="fixed top-0 left-0 z-50 w-full h-full bg-gray-800 bg-opacity-50 flex justify-end overflow-hidden">
+      <div className="w-1/4 p-[1rem] bg-white">
+        <div className="flex items-center mb-[2rem]">
+          <h1 className="text-xl flex-1 text-center">Buscar...</h1>
+          <FontAwesomeIcon icon={faXmark} className="ml-auto w-3 cursor-pointer" onClick={handleCloseModal} />
         </div>
-    )
+        <div className="border p-[1rem] mb-[1rem]">
+          <select className="w-full focus:outline-none" value={selectedCategory} onChange={handleCategoryChange} >
+            <option value="default">Todas las categorias</option>
+            {categories.map((category) => (
+              <option value={category.id} key={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="border p-[1rem] mb-[1rem]">
+          <form className="flex items-center" onSubmit={handleSearch}>
+            <input type="text" placeholder="Buscar artículos" className="focus:outline-none" value={searchText} onChange={handleInputChange} />
+            <button type="submit" className="ml-auto w-4">
+              <FontAwesomeIcon icon={faSearch}/>            
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
