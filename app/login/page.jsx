0e8bc59416c521a1c1744777593b8596a2009f8e
Magui Martin <magalimartin02@gmail.com>
2024-04-14 18:01:03 -0300

    form register

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react';
-import { Formik, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export default function Login() {
     return (
@@ -9,47 +9,39 @@ export default function Login() {
                 validate={(values) => {
                     const errors = {}
                     if (!values.email) {
-                        errors.email = "Se requiere un email"
+                        errors.email = "Por favor ingresa un email."
                     } else if (
                         !/^[\w-]+(\.[\w-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/.test( values.email)
-                    ) {
-                        errors.email = "Formato de email invalido"
-                    }
+                    ) {errors.email = "Formato de email invalido."}
+
                     if (!values.password) {
-                        errors.password = "Se requiere una contraseña"
+                        errors.password = "Por favor ingresa tu contraseña."
                     } else if (values.password.length < 8) {
-                        errors.password =
-                            "La contraseña debe tener al menos 8 caracteres."
-                    }
+                        errors.password = "La contraseña debe tener al menos 8 caracteres."}
                     return errors
                 }}
         >
-            {({values, errors, handleSubmit, handleChange, handleBlur}) => (
-                <form onSubmit={handleSubmit}> 
+            {({handleSubmit}) => (
+                <Form onSubmit={handleSubmit}> 
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur}/>
+                        <Field type="email" id="email" name="email" />
                         <div>
-							<ErrorMessage name="email" component={() => (
-								<p className="text-red-500 text-xm italic">
-												{errors.email}
-							    </p>
-								)}/>
+                            <ErrorMessage name="email" component="p" className="text-red-500 text-sm italic" />
 						</div>
                     </div>
                     <div>
                         <label htmlFor="password">Contraseña</label>
-                        <input type="text" id="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur}/>
+                        <Field type="password" id="password" name="password"/>
                         <div>
-							<ErrorMessage name="password3.." component={() => (
-								<p className="text-red-500 text-xm italic">
-												{errors.password}
-							    </p>
-								)}/>
+                            <ErrorMessage name="password" component="p" className="text-red-500 text-sm italic" />
 						</div>
                     </div>
+                    <div>
+                        <h1>¿Olvidaste tu contraseña?</h1>
+                    </div>
                     <button type="submit" >Ingresar</button>
-                </form>
+                </Form>
             )}
         </Formik>        
     );
