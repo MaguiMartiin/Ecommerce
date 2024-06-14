66a451223aa44a6d48509c2630f9449f28daa903
Magui Martin <magalimartin02@gmail.com>
2024-06-13 21:54:24 -0300

    add buttons in the sideCart

---
+++ app/cart/page.jsx
@@ -0,0 +1,7 @@
+export default function Cart () {
+    return(
+        <div className="pt-[5rem]">
+            <h1>Mi carrito </h1>
+        </div>
+    )
+}
--- components/SideCart.jsx
+++ components/SideCart.jsx
@@ -59,7 +59,7 @@ export const SideCart = () => {
                         <h1 className="text-xl flex-1 text-center">Tu carrito</h1>
                         <FontAwesomeIcon icon={faXmark} className="ml-auto w-3 cursor-pointer" onClick={closeSideCart} />
                     </div>
-                    <div className='overflow-y-auto h-2/3 w-full'>
+                    <div className='overflow-y-auto h-3/5 w-full scrollbar-thumb-rounded'>
                         {product.productItems.length === 0 ? (
                             <div className="flex flex-col items-center justify-center h-full ">
                                 <h1 className='mb-[2rem]'>Tu carrito está vacío</h1>
@@ -69,13 +69,13 @@ export const SideCart = () => {
                             </div>
                         ) : (
                             product.productItems.map((item) => (
-                            <div key={item.id} className='flex p-[1rem] border-b-2'>
+                            <div key={item.id} className='flex p-[1rem] border-b-2 '>
                                 <img src={item.image} alt={item.name} className=' w-1/3 h-2/3' />
                                 <div className='flex flex-col p-[1rem] w-full space-y-6 justify-center'>
                                     <div>
                                         <h1>{item.name} - {item.size}</h1>
                                         <h2>{item.color}</h2>
-                                        <p>{item.price}</p>
+                                        <p>${item.price}</p>
                                     </div>
                                     <div className='flex justify-between items-center'>
                                         <Counter  maxCount={item.maxQuantity} count={item.quantity} onCountChange={(newCount) => handleChangeQuantity(item.id, item.size, item.color, newCount)} w={'100px'} h={'50px'} />
@@ -92,9 +92,17 @@ export const SideCart = () => {
                     )}
                     </div>
                     {product.productItems.length > 0 && (
-                        <div className='flex items-center justify-between p-[1.5rem] '>
-                            <h1>Subtotal: </h1>
-                            <h1>$ {calculateSubtotal().toFixed(2)}</h1>
+                        <div>
+                            <div className='flex items-center justify-between p-[1rem] bg-slate-200 '>
+                                <h1>Subtotal: </h1>
+                                <h1>$ {calculateSubtotal().toFixed(2)}</h1>
+                            </div>
+                            <div className='flex flex-col p-[1rem] space-y-5'>
+                                <Link href='/cart' className='bg-cherry_blossom_pink h-12 hover:shadow-xl transition-shadow duration-500 text-white justify-center flex' onClick={closeSideCart}>
+                                    <button>Ver carrito</button>
+                                </Link>
+                                <button className='bg-white h-12 border border-black hover:shadow-xl transition-shadow duration-500'>Finalzar compra</button>
+                            </div>
                         </div>
                     )}
                 </div>
